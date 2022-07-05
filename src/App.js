b9b9737aa4a5d0c4e4bc2f405cb570a35c1dde2c
unknown <rashedul15-1718@diu.edu.bd>
2022-07-04 22:07:12 -0700

    Code Updated

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function Counter(){
 
 
 function Users(){
-  const [users, setUsers] = useState({});
+  const [users, setUsers] = useState([]);
   useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
